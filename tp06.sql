b2be1a44f137a24521dca02714f5d23edc3afb3a
ALABDULKADER <amensy111a@gmail.com>
2025-04-16 14:22:38 +0200

    Tp Requetes  Sql

---
+++ tp06.sql
@@ -0,0 +1,12 @@
+-- a
+DELETE compo
+FROM diginamic.compo
+         JOIN diginamic.bon ON compo.ID_BON = bon.ID
+WHERE YEAR(bon.DATE_CMDE) = '2019'
+  AND MONTH(bon.DATE_CMDE) = 4;
+  
+-- b
+DELETE
+FROM diginamic.bon
+WHERE YEAR(bon.DATE_CMDE) = '2019'
+  AND MONTH(bon.DATE_CMDE) = 4;
